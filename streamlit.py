2bf7faefc2a4ea2502df463a66851eff350b0e36
WilliamWestrup <williamwestrup@gmail.com>
2023-03-04 01:49:02 -0300

    update

--- streamlit.py
+++ streamlit.py
@@ -2,39 +2,60 @@ import openai
 import json
 import os
 import streamlit as st
+import streamlit.components.v1 as components
 
 openai.api_key = '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'
 
 modelo = "text-davinci-002"
 
-def gerar_recomendacao(gostei, n_gostei):
+def gerar_recomendacao(gostei):
     resposta = openai.Completion.create(
         engine=modelo,
-        prompt=f"Considerando que eu gosto dos filmes {gostei} e não gosto dos filmes {n_gostei}, me recomende um filme do meu gosto. Quero que a resposta seja em portugues e venha no formato de dicionário python, contendo o nome do filme, a nota do imdb, uma sinopse, imagem do cartaz e o motivo do filme ser recomendado.",
+        prompt=f"Considerando que eu assisti e gostei dos filmes {gostei}, me recomende um filme pouco conhecido que eu vá gostar e ainda não assisti. Quero que a resposta seja em portugues e venha no formato de dicionário python, contendo o nome do filme, a nota do imdb, uma sinopse, link de uma imagem do filme no google e o motivo do filme ser recomendado.",
         max_tokens=1000,
         n=1,
         stop=None,
-        temperature=0.7,
-        presence_penalty=0,
-        frequency_penalty=0,
+        temperature=0.6,
+        presence_penalty=0.8,
+        frequency_penalty=0.3,
     )
 
-    dict_resposta = json.loads(resposta.choices[0].text.replace(".\n\n","").replace("'", '"'))
+    dict_resposta = json.loads('{"nome"'+resposta.choices[0].text.replace("'",'"').split('"nome"')[1].replace("\n",''))
 
     return dict_resposta
 
 st.title("Recomendações de filmes")
 
-gostei = st.multiselect("Quais filmes você gostou?", ["Interstelar", "Capitão Fantástico", "Batman", "Superman"])
-n_gostei = st.multiselect("Quais filmes você não gostou?", ["Harry Potter", "O Enigma de Outro Mundo", "Senhor dos Anéis"])
+gostei = st.multiselect(f"Quais filmes você gostou?", ["O Poderoso Chefão",
+    "O Senhor dos Anéis: O Retorno do Rei",
+    "Forrest Gump: O Contador de Histórias",
+    "Star Wars: Episódio V - O Império Contra-Ataca",
+    "O Rei Leão",
+    "Os Caçadores da Arca Perdida",
+    "De Volta para o Futuro",
+    "Clube da Luta",
+    "Matrix",
+    "Pulp Fiction: Tempo de Violência",
+    "A Lista de Schindler",
+    "Batman: O Cavaleiro das Trevas",
+    "Os Suspeitos",
+    "O Labirinto do Fauno",
+    "Cidade de Deus"])
+
+col1, col2 = st.columns([1, 2])
+
+def icon_text(text):
+    components.html(f'<i class="fa-brands fa-imdb"></i> {text}', height=30)
+
 
 if st.button("Gerar recomendação"):
-    if not gostei or not n_gostei:
+    if not gostei:
         st.warning("Selecione pelo menos um filme que você gostou e um que você não gostou.")
     else:
-        recomendacao = gerar_recomendacao(gostei, n_gostei)
-        st.write(f"Recomendação: {recomendacao['nome_do_filme']}")
-        st.write(f"Nota do IMDb: {recomendacao['nota_imdb']}")
-        st.write(f"Sinopse: {recomendacao['sinopse']}")
-        st.image(recomendacao['imagem_do_cartaz'], caption="Cartaz do filme recomendado")
-        st.write(f"Motivo da recomendação: {recomendacao['motivo']}")
+        recomendacao = gerar_recomendacao(gostei)
+        col1.image(list(recomendacao.values())[3], caption="Cartaz do filme recomendado")
+        col2.write(f"Você provávelmente gostará de: **{list(recomendacao.values())[0]}**")
+        col2.write(f"**Nota do IMDb:** {list(recomendacao.values())[1]}")
+        #col2.write(icon_text('teste'))
+        col2.write(f"**Sinopse:** {list(recomendacao.values())[2]}")
+        col2.write(f"**Motivo da recomendação:** {list(recomendacao.values())[4]}")
